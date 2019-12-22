55157f5a5a4a628f41b2bb6a1ac057fd7418451b
rj <rizu1023@gmail.com>
2019-12-22 20:20:00 +0530

    rocket
    upward movement

---
+++ 3_Project_boost/Assets/rocket.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour {
+	Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+	rigidBody=GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessInput();	
+	}
+	void ProcessInput()
+	{
+		if(Input.GetKey(KeyCode.Space))
+		{
+			rigidBody.AddRelativeForce(Vector3.up);
+		}
+		 if(Input.GetKey(KeyCode.A))
+		{
+			print("left rotation.");
+		}
+		else if(Input.GetKey(KeyCode.D))
+		{
+			print("right rotation.");
+		}
+		
+	}
+}
